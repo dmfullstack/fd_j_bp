cc83792c1119859561689e61d6444d52cdb04707
Dinesh Metkari <metkaridinesh@gmail.com>
2018-06-06 17:59:02 +0530

    ic

---
+++ Server/src/main/java/com/stackroute/foodieApp/FoodieApp.java
@@ -0,0 +1,10 @@
+package com.stackroute.foodieApp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class FoodieApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodieApp.class, args);
+	}
+}
